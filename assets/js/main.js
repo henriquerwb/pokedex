a65c2f4b5a46717bbf9262a58aeb5a54b1d20d21
henriquerwb <h3nriquegomes@gmail.com>
2023-12-17 11:55:09 -0300

    feat: melhorado consumo da api

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,24 @@
-console.log('Hello World!');
+function convertPokemonToHtml(pokemon) {
+    return `
+        <li class="pokemon">
+                <span class="number">#001</span>
+                <span class="name">${pokemon.name}</span>
+                    
+                <div class="detail">
+                    <ol class="types">
+                        <li class="type">Grass</li>
+                        <li class="type">Poison</li>
+                    </ol>
+
+                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="${pokemon.name}">
+                </div>
+            </li>
+    `
+}
+
+const pokemonOl = document.getElementById('pokemonList');
+
+
+pokeApi.getPokemons().then((pokemonList = []) => {
+    pokemonOl.innerHTML += pokemonList.map(convertPokemonToHtml).join('');
+})
